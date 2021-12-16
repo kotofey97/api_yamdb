f4572e946f871b5519d0a96f51e78fcc6d641859
kotofey97 <olya_sitnikova@mail.ru>
2021-12-16 21:01:38 +0300

    serializers addes

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from .models import Category, Genre, Title
+
+
+User = get_user_model()
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Title
+        fields = '__all__'
