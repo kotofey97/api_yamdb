84d3a49ce18475b202b854b9fd321a095df2e47f
kotofey97 <olya_sitnikova@mail.ru>
2022-01-10 15:36:26 +0300

    reviews, comments models add

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from api.validators import year_validator
 
@@ -80,3 +81,62 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    text = models.TextField()
+    score = models.IntegerField(
+        blank=True,
+        validators=[
+            MinValueValidator(1, message='Слишком низкая оценка'),
+            MaxValueValidator(10, message='Слишком высокая оценка'),
+        ]
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва'
+    )
+    pub_date = models.DateField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления отзыва'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария на отзыв'
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    pub_date = models.DateField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления комментария'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
-
-from .models import Category, Genre, Title
-
+from rest_framework.generics import get_object_or_404
+from .models import Category, Comment, Genre, Review, Title
 
 User = get_user_model()
 
@@ -23,7 +21,46 @@ class CategorySerializer(serializers.ModelSerializer):
 class TitleSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     genre = GenreSerializer(read_only=True, many=True)
+    rating = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Title
         fields = '__all__'
+        read_only_fields = ['id', 'rating']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    def validate(self, data):
+        author = self.context['request'].user
+        title = get_object_or_404(
+            Title,
+            id=self.context['request'].parser_context['kwargs'].get('title_id')
+        )
+        if (self.context['request'].method == 'POST'
+                and title.reviews.filter(author=author).exists()):
+            raise serializers.ValidationError(
+                f'Отзыв на произведение {title.name} уже существует'
+            )
+        return data
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+    review = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
